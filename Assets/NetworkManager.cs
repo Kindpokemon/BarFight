e8b6f703c37e71ec5d9bc9af035589ef3727f6a5
Kindpokemon <kindpokemon@gmail.com>
2015-07-29 14:05:15 -0500

    Ohgodwhat

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Connect ();	
+	}
+
+	void Connect(){
+		PhotonNetwork.ConnectUsingSettings( "v0.0.1");
+	}
+
+	void OnGUI(){
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
+
+	}
+
+	void OnJoinedLobby() {
+		PhotonNetwork.JoinRandomRoom ();
+	}
+}
